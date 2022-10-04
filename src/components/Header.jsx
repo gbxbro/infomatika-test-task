8b974ac0dfcefa10226772fddfb548b00932de0a
Dmitry Gavrilov <gavrstyle@gmail.com>
2022-10-04 17:58:02 +0300

    Feature: added jsx layout for Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
+        <header className="header">
+            <nav className="header__menu menu">
+                <Link className="menu__link" to="/">
+                    Билеты и абонементы
+                </Link>
+                <ul className="menu__list">
+                    <li className="menu__item">
+                        <Link className="menu__link" to="#">Как купить?</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link className="menu__link" to="#">Правила</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link className="menu__link" to="#">Возврат билетов</Link>
+                    </li>
+                </ul>
+                <button className="header__button">
+                    Войти
+                </button>
+                <button className="burger-menu">
+                    burger-menu
+                </button>
+            </nav>
             Header
-        </div>
+        </header>
     );
 };
 
